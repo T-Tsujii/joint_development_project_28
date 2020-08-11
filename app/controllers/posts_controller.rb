9393a474ba2c16907f2e0fec7ecca04a4b0ee5c9
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-08-11 15:47:20 +0900

    編集機能完了。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,16 @@ class PostsController < ApplicationController
     Post.create(post_params)
   end
 
+  def edit
+    @post = Post.find(params[:id])  
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)  
+  end
+
+
   private
   def post_params
     params.require(:post).permit(:title, :content)

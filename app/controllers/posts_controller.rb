701a1b7112d4256f34c34e41a6a489aa5b1bafb9
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-08-11 11:57:27 +0900

    一覧表示機能完了。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,12 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all  
+  end
+  
   def new
     @post = Post.new
   end
-  
+
   def create
     Post.create(post_params)
   end
